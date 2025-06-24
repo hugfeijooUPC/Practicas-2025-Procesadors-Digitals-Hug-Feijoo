5ae77fe411f0d6592e01db39e14ae67e30488789
Hug Feijoo <hug.feijoo@estudiantat.upc.edu>
2025-06-24 18:11:24 +0200

    Delete Proyecto Final - Contador de Objetos directory

--- Proyecto Final - Contador de Objetos/src/SensorTest.h
+++
@@ -1,9 +0,0 @@
-#ifndef SENSORTEST_H
-#define SENSORTEST_H
-
-#include "SensorManager.h"
-
-void setupSensorTest(SensorManager& sensor);
-void loopSensorTest(SensorManager& sensor);
-
-#endif // SENSORTEST_H
--- Proyecto Final - Contador de Objetos/src/WebServerTest.h
+++
@@ -1,9 +0,0 @@
-#ifndef WEBSERVERTEST_H
-#define WEBSERVERTEST_H
-
-#include "WebServerManager.h"
-
-void setupWebServerTest(WebServerManager& server, int* counterPtr);
-void loopWebServerTest(WebServerManager& server);
-
-#endif // WEBSERVERTEST_H
